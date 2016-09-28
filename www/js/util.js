88207b0b25d2084bc0e0562e5e0130ff874d47ef
Joe Kachmar <joseph.kachmar@gmail.com>
2016-09-28 14:48:10 -0400

    Removes deprecated websocket stuff

--- www/js/util.js
+++ www/js/util.js
@@ -69,62 +69,6 @@ var util = (function() {
     return payload;
   }
 
-/** Websocket Stuff ----------------------------------------------------------*/
-  var socket;
-
-  function createSocket(url, slug) {
-    if (socket && socket.io.uri === url) {
-      if (socket.connected) {
-        console.log('Socket connected');
-        return;
-      }
-    }
-
-    if (socket) {
-      socket.close();
-      socket.removeAllListeners();
-      socket.doConnect = true;
-      console.log('Socket reset');
-    }
-
-    socket = io(url, {
-      transports: [
-        'polling',
-        'websocket',
-        'htmlfile',
-        'xhr-polling',
-        'jsonp-polling',
-      ],
-    });
-
-    // If we're connected, enable form submission
-    socket.on('connect', function () {
-      console.log('Connected');
-      SUBMIT_BUTTON_LIST.forEach(function(each) {
-        document.getElementById(each).removeAttribute('disabled', '');
-      });
-    });
-
-    // If there was a connection error, disable form submission
-    socket.on('error', function(e){
-      ons.notification.alert('Error connecting to Menagerie!');
-      SUBMIT_BUTTON_LIST.forEach(function(each) {
-        document.getElementById(each).setAttribute('disabled', '');
-      });
-    });
-
-    socket.on(slug, function(payload){
-      console.log('UPDATED: ', JSON.stringify(payload));
-      var response = payload.success ? 'All Good!' : 'Error';
-      ons.notification.alert(response);
-    });
-
-    if (socket.doConnect) {
-      socket.connect();
-      console.log('Socket connecting');
-    }
-  }
-
   return {
     get: getFromMenagerie,
     post: postToMenagerie,
